58989d99e2bd5469da203a78f3332570327794f3
Yuri Boblak <160606809+sselesnes@users.noreply.github.com>
2025-03-14 02:39:21 +0200

    added sessionStorage

--- js/task-6.js
+++ js/task-6.js
@@ -1,10 +1,12 @@
 const inputField = document.querySelector("#controls input");
 const createBtn = document.querySelector("[data-create]");
 const destroyBtn = document.querySelector("[data-destroy]");
+const exploreBtn = document.querySelector("[data-explore]");
 const boxesDiv = document.querySelector("#boxes");
 
 inputField.focus();
 let boxesAmount;
+let boxesArray = [];
 
 const getRandomHexColor = () =>
   `#${Math.floor(Math.random() * 16777215)
@@ -14,24 +16,72 @@ const getRandomHexColor = () =>
 const createBoxes = amount => {
   const boxesHTML = Array.from({ length: amount }, (_, index) => {
     const size = 30 + index * 10;
-    return `<div style="width: ${size}px; height: ${size}px; background-color: ${getRandomHexColor()};"></div>`;
+    const randomColor = getRandomHexColor();
+    boxesArray.push(randomColor);
+    return `<div style="width: ${size}px; height: ${size}px; background-color: ${randomColor};"></div>`;
   }).join("");
   boxesDiv.innerHTML = boxesHTML;
 };
 
 ["click", "pointerover", "pointerout"].forEach(eventType => {
   createBtn.addEventListener(eventType, () => {
-    if (eventType === "click") boxesAmount && createBoxes(boxesAmount);
+    if (eventType === "click") {
+      if (boxesAmount) {
+        createBoxes(boxesAmount);
+        sessionStorage.setItem(
+          `boxesArray${sessionStorage.length}`,
+          JSON.stringify(boxesArray)
+        );
+        boxesArray.length = 0;
+      }
+    }
     if (eventType === "pointerover") boxesAmount && (createBtn.style.background = "#6C8Cff");
     if (eventType === "pointerout")
       setTimeout(() => (createBtn.style.background = "#4E75FF"), 100);
   });
 });
 
+["click", "pointerover", "pointerout"].forEach(eventType => {
+  exploreBtn.addEventListener(eventType, () => {
+    if (eventType === "click" && sessionStorage.length > 1) {
+      boxesDiv.innerHTML = "";
+      const keys = Object.keys(sessionStorage);
+      const values = Object.values(sessionStorage);
+      let keyValuePairs = keys.map((key, index) => ({ key, value: values[index] }));
+      keyValuePairs = keyValuePairs
+        .filter(pair => pair.key !== "IsThisFirstTime_Log_From_LiveServer")
+        .sort((a, b) => a.key.localeCompare(b.key));
+
+      const table = document.createElement("table");
+      table.border = 1;
+
+      const header = table.createTHead();
+      const row = header.insertRow(0);
+      row.insertCell(0).outerHTML = "<th>Key</th>";
+      row.insertCell(1).outerHTML = "<th>Value</th>";
+
+      const body = table.createTBody();
+      keyValuePairs.forEach((pair, index) => {
+        const row = body.insertRow(index);
+        row.insertCell(0).innerText = pair.key;
+        row.insertCell(1).innerText = pair.value;
+      });
+
+      boxesDiv.appendChild(table);
+    }
+    if (eventType === "pointerover")
+      sessionStorage.length > 1 && (exploreBtn.style.background = "#6C8Cff");
+    if (eventType === "pointerout")
+      setTimeout(() => (exploreBtn.style.background = "#4E75FF"), 100);
+  });
+});
+
 ["click", "mouseover", "mouseout"].forEach(eventType => {
   destroyBtn.addEventListener(eventType, () => {
-    if (eventType === "click")
+    if (eventType === "click") {
+      boxesArray.length = 0;
       createBoxes(), setTimeout(() => (destroyBtn.style.background = "#ff4e4e"), 100);
+    }
     if (eventType === "mouseover")
       boxesDiv.innerHTML && (destroyBtn.style.background = "#ff7070");
     if (eventType === "mouseout") destroyBtn.style.background = "#ff4e4e";
@@ -66,7 +116,7 @@ document.querySelector("body").addEventListener("keydown", () => {
 // includes external tinycolor library
 
 boxesDiv.addEventListener("click", event => {
-  if (event.target !== event.currentTarget) {
+  if (event.target !== event.currentTarget && event.target.nodeName !== "TD") {
     const currentColor = event.target.style.backgroundColor;
     const currentIndex = Array.from(boxesDiv.querySelectorAll("div")).indexOf(event.target);
 
